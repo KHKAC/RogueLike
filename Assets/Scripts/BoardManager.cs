3ab44c3bb6d9eef232945508e46c919d96f23f24
KHKAC <rudgnsrb888@naver.com>
2025-12-12 13:26:13 +0900

    251212 Add Food

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Tilemaps;
 
@@ -14,16 +15,20 @@ public class BoardManager : MonoBehaviour
     public Tile[] groundTiles;
     public Tile[] wallTiles;
     public PlayerController player;
+    public GameObject foodPrefab;
 
     Tilemap tilemap;
     CellData[,] boardData;
     Grid grid;
+    List<Vector2Int> emptyCellList;
     
     public void Init()
     {
         tilemap = GetComponentInChildren<Tilemap>();
         grid = GetComponentInChildren<Grid>();
+        emptyCellList = new List<Vector2Int>();
         boardData = new CellData[width, height];
+
         for(int y = 0; y < height; y++)
         {
             for(int x = 0; x < width; x++)
@@ -41,11 +46,14 @@ public class BoardManager : MonoBehaviour
                     // tile = groundTiles[Random.Range(0, groundTiles.Length)];
                     tile = GetRandomTile(groundTiles);
                     boardData[x,y].passable = true;
+                    emptyCellList.Add(new Vector2Int(x, y));
                 }
                 tilemap.SetTile(new Vector3Int(x, y, 0), tile);
             }
         }
         // player.Spawn(this, new Vector2Int(1, 1));
+        emptyCellList.Remove(new Vector2Int(1, 1));
+        GenerateFood();
     }
 
     Tile GetRandomTile(Tile[] tiles)
@@ -67,5 +75,21 @@ public class BoardManager : MonoBehaviour
         }
         
         return boardData[cellIndex.x, cellIndex.y];
+    }
+
+    void GenerateFood()
+    {
+        int foodCount = 5;
+        for(int i = 0; i < foodCount; i++)
+        {
+            int randomIndex = Random.Range(0, emptyCellList.Count);
+            Vector2Int coord = emptyCellList[randomIndex];
+
+            emptyCellList.RemoveAt(randomIndex);
+            CellData data = boardData[coord.x, coord.y];
+            GameObject newFood = Instantiate(foodPrefab);
+            newFood.transform.position = CellToWorld(coord);
+            data.containedObject = newFood;
+        }
     }
 }
